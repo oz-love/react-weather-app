bd8a0e8e78e1178d5164af3a839f016fd1c21884
oz-love <53973603+oz-love@users.noreply.github.com>
2022-05-03 23:25:19 +0900

    build1

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ const Form = (props) => {
 
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
+            <input type="text" name="city" placeholder="cityname" onChange={e => props.setCity(e.target.value)} />
             <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
